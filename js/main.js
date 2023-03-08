e8849b8df122436948461c574500b0e490288e61
Dhart1984 <donalde.hart@gmail.com>
2023-03-08 16:36:08 -0500

    last day push

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
-                                // constants 
+// constants 
+
+
+
 //apps state (variables) //what data the application of the game needs to render
         //playerScore, rightAnswer
 let playerScore;
 let skipQuestion;
-let wrongScore;
+let strikeScore;
                                 //cached elements 
 
 //cached scores
 const pScoreEl = document.querySelector("#p-score");
-const sScoreEl = document.querySelector("#s-score");
-const wScoreEl = document.querySelector("#w-score");
+const skpScoreEl = document.querySelector("#skip-score");
+const strScoreEl = document.querySelector("#strike-score");
 
 //cached buttons 
 const btnEls = document.querySelectorAll("#q-button");
 const tbtnEl = document.querySelector('#true-button');
 const fbtnEl = document.querySelector('#false-button');
 const sbtnEl = document.querySelector('#start-button');
+const qbtnEl = document.querySelector('#questions')
 
                                 //event listeners
 
 function handleClick(evt) {
     const playerChoice = evt.target.textContent
     const newQuestion = generateQuestion();
-    //const trueChoice = 
-    console.log(playerChoice, newQuestion)
+    //trueChoice();
+    checkWinner(playerChoice, newQuestion)
 }
 
 btnEls.forEach(function (btn){
@@ -31,15 +35,16 @@ btnEls.forEach(function (btn){
     console.log(btn);
 })
 
-tbtnEl.addEventListener('click', function(){
-    console.log('This is True');
+//let trueChoice =
+tbtnEl.addEventListener('click true', function(){
+    console.log("this is true");
 })
 
-fbtnEl.addEventListener('click', function(){
+fbtnEl.addEventListener('click false', function(){
     console.log("This is False");
 })
 
-sbtnEl.addEventListener('click', function(){
+sbtnEl.addEventListener('click start', function(){
     console.log("I'm Ready to Play!");
 })
                                 //functions 
@@ -47,8 +52,8 @@ sbtnEl.addEventListener('click', function(){
 function init(){
     console.log('starting game')
     playerScore = 0;
-    skipQuestion = 0;
-    wrongScore = 0;
+    skipQuestion = 2;
+    strikeScore = 3;
 
     render();
 }
@@ -61,8 +66,8 @@ function render(){
 function renderScores(){
     //update cached dom elements - scoreEls
     pScoreEl.textContent = playerScore;
-    sScoreEl.textContent = skipQuestion;
-    wScoreEl.textContent = wrongScore;
+    skpScoreEl.textContent = skipQuestion;
+    strScoreEl.textContent = strikeScore;
 }
 
 function renderResults(){
@@ -71,14 +76,19 @@ function renderResults(){
 
 function generateQuestion (){
     const questionList = [
-        "True or False: The largest country in the world by land area is Russia.",
-        "True or False: The capital of France is Paris.",
-        "True or False: The continent of South America is located entirely in the Southern Hemisphere.",
-        "True or False: The Great Barrier Reef is located off the coast of Australia.",
-        "True or False: The highest mountain in North America is Denali (formerly known as Mount McKinley).",
-    ]
-    const randomQuestion = Math.floor(Math.random()*questionList.length);
-    return questionList[randomQuestion]
+        { question: "True or False: The largest country in the world by land area is Russia.", answer: "true" },
+        { question: "True or False: The capital of France is Paris.", answer: "true" },
+        { question: "True or False: The continent of South America is located entirely in the Southern Hemisphere.", answer: "true" },
+        { question: "True or False: The Great Barrier Reef is located off the coast of Australia.", answer: "true" },
+        { question: "True or False: The highest mountain in North America is Denali (formerly known as Mount McKinley).", answer: "true" }
+    ];
+
+const randomQuestion = Math.floor(Math.random()*questionList.length);
+return questionList[randomQuestion]
+}
+
+function checkWinner (p1, p2) {
+    console.log('check winner', p1, p2)
 }
 
 init();
