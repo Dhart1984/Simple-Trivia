5b98c92e02bd47532ff675984390b3b2fc956fd6
Dhart1984 <donalde.hart@gmail.com>
2023-03-07 23:08:52 -0500

    adjusted html v2, added questions, cached to only questions button el

--- js/main.js
+++ js/main.js
@@ -12,7 +12,8 @@ const sScoreEl = document.querySelector("#s-score");
 const wScoreEl = document.querySelector("#w-score");
 
 //cached buttons 
-const btnEls = document.querySelectorAll("button");
+const btnEls = document.querySelectorAll("#q-button");
+//new add - const tbtnEl = document.querySelector('#true-button')
 
                                 //event listeners
 
@@ -23,9 +24,7 @@ function handleClick(evt) {
 }
 
 btnEls.forEach(function (btn){
-
     btn.addEventListener('click', handleClick)
-
     console.log(btn);
 })
 
@@ -57,7 +56,13 @@ function renderResults(){
 }
 
 function generateQuestion (){
-    const questionList = ['q1', 'q2', 'q3', 'q4', 'q5']
+    const questionList = [
+        "True or False: The largest country in the world by land area is Russia.",
+        "True or False: The capital of France is Paris.",
+        "True or False: The continent of South America is located entirely in the Southern Hemisphere.",
+        "True or False: The Great Barrier Reef is located off the coast of Australia.",
+        "True or False: The highest mountain in North America is Denali (formerly known as Mount McKinley).",
+    ]
     const randomQuestion = Math.floor(Math.random()*questionList.length);
     return questionList[randomQuestion]
 }
--- js/stash.js
+++ js/stash.js
@@ -1,7 +1,16 @@
 // creating the objects and object arrays 
 
 function gernerateQuestion (){
-    const questionList = ['q1', 'q2', 'q3', 'q4', 'q5']
+    const questionList = [
+        "True or False: The largest country in the world by land area is Russia.",
+        "True or False: The capital of France is Paris.",
+        "True or False: The continent of South America is located entirely in the Southern Hemisphere.",
+        "True or False: The Great Barrier Reef is located off the coast of Australia.",
+        "True or False: The highest mountain in North America is Denali (formerly known as Mount McKinley).",
+    ]
     const randomQuestion = Math.floor(Math.random()*questionList.length);
     return questionList[randomQuestion]
+
+
+
 }
